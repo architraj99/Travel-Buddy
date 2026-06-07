b342550045ebd477fd392c1eedd39594104d7527
architraj99 <rajarchit99@gmail.com>
2026-06-07 20:19:19 +0530

    Added date difference calculation for upcoming trips

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ function addTrip() {
         return;
     }
 
+    let daysLeft = getDaysLeft(date);
+
     let card = document.createElement("div");
     card.className = "trip-card";
 
@@ -26,11 +28,23 @@ function addTrip() {
         <h3>${place}</h3>
         <p>Date: ${date}</p>
         <p>Budget: ₹${budget}</p>
-    `;
 
+        <p class="days-left">${daysLeft} Days Left</p>
+    `;
+   
     tripList.appendChild(card);
 
     placeInput.value = "";
     dateInput.value = "";
     budgetInput.value = "";
+}
+
+function getDaysLeft(date) {
+
+    let today = new Date();
+    let tripDate = new Date(date);
+
+    let diff = tripDate - today;
+
+    return Math.ceil(diff / (1000 * 60 * 60 * 24) );
 }
